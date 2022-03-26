c77bc4666cdc408585e642c5a1830f21e7fb867d
supaakbar <msupanjiakbar@gmail.com>
2022-03-26 23:49:05 +0700

    add section container CPU & MEM usage

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask, render_template
 import platform
 import os
 from datetime import datetime
+import docker
 
 app = Flask(__name__)
+client = docker.from_env()
+stats = client.containers.get('trial-app').stats(stream=False)
 
 
 @app.route('/')
@@ -11,7 +14,20 @@ def index():
     getOs = str(platform.platform())
     getVersion = str(os.environ['APP_VERSION'])
     getDate = str(datetime.today().strftime('%d-%m-%Y'))
-    return render_template('index.html', OS=getOs, VERSION=getVersion, DATE=getDate)
+    return render_template('index.html', OS=getOs, VERSION=getVersion, DATE=getDate, CPU=getCpu(), MEM=getMem())
+
+def getCpu():
+    usageDelta = stats['cpu_stats']['cpu_usage']['total_usage'] - stats['precpu_stats']['cpu_usage']['total_usage']
+    systemDelta = stats['cpu_stats']['system_cpu_usage'] - stats['precpu_stats']['system_cpu_usage']
+    len_cpu = len(stats['cpu_stats']['cpu_usage']['percpu_usage'])
+    percentage = (usageDelta / systemDelta) * len_cpu * 100
+    percent = round(percentage, 2)
+    return percent
+
+def getMem():
+    usage = stats['memory_stats']['usage']
+    convertion = usage / 1000000
+    return convertion
 
 if __name__ == '__main__':
     app.run()
