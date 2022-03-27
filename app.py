6c76b38562657e2fd686af4251c201e546d6107a
supaakbar <msupanjiakbar@gmail.com>
2022-03-28 01:09:14 +0700

    final update for all version

--- app.py
+++ app.py
@@ -7,10 +7,17 @@ import logging
 import sys
 
 app = Flask(__name__)
+
+# linux command export env
+cmdGetContainerID = 'export CONTAINER_ID=$(cat /proc/self/cgroup | grep -o  -e "docker-.*.scope" | head -n 1 | sed "s/docker-\(.*\).scope/\\1/")'
+os.system(cmdGetContainerID)
+
+# docker command
 client = docker.from_env()
-# client = client(base_url='unix://var/run/docker.sock')
-stats = client.containers.get('trial-app').stats(stream=False)
-logging.basicConfig(filename='app.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s : %(message)s')
+stats = client.containers.get(os.environ['CONTAINER_ID']).stats(stream=False)
+
+# logging
+logging.basicConfig(filename='./logs/app.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s : %(message)s')
 
 
 @app.route('/')
@@ -23,6 +30,8 @@ def index():
 
 @app.route("/getcpu/", methods=['GET'])
 def getCpu():
+    client = docker.from_env()
+    stats = client.containers.get(os.environ['CONTAINER_ID']).stats(stream=False)
     usageDelta = stats['cpu_stats']['cpu_usage']['total_usage'] - stats['precpu_stats']['cpu_usage']['total_usage']
     systemDelta = stats['cpu_stats']['system_cpu_usage'] - stats['precpu_stats']['system_cpu_usage']
     len_cpu = len(stats['cpu_stats']['cpu_usage']['percpu_usage'])
@@ -32,6 +41,8 @@ def getCpu():
 
 @app.route("/getmem/", methods=['GET'])
 def getMem():
+    client = docker.from_env()
+    stats = client.containers.get(os.environ['CONTAINER_ID']).stats(stream=False)
     usage = stats['memory_stats']['usage']
     convertion = str(usage / 1000000)
     return convertion
