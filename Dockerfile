547c95cbc37ad91f077239d93ce04b000b9f72a8
supaakbar <msupanjiakbar@gmail.com>
2022-03-27 19:07:51 +0700

    add logging feature

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,19 @@
 # base image
 FROM python:3.10-alpine
 
-# configure app version
+# # install gcc
+RUN apk update && apk upgrade
+RUN apk add --no-cache \
+                    gcc \
+                    musl-dev \
+                    linux-headers \
+                    python3-dev \
+    && rm -rf /var/cache/apk/*
+
+# configure env var
 ARG DOCKER_TAG
 ENV APP_VERSION=$DOCKER_TAG
+ENV TZ="Asia/Jakarta"
 
 # setup working directory
 WORKDIR /trial-app
--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import platform
 import os
 from datetime import datetime
 import docker
+import logging
+import sys
 
 app = Flask(__name__)
 client = docker.from_env()
+# client = client(base_url='unix://var/run/docker.sock')
 stats = client.containers.get('trial-app').stats(stream=False)
+logging.basicConfig(filename='app.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s : %(message)s')
 
 
 @app.route('/')
@@ -14,20 +18,23 @@ def index():
     getOs = str(platform.platform())
     getVersion = str(os.environ['APP_VERSION'])
     getDate = str(datetime.today().strftime('%d-%m-%Y'))
+    app.logger.info('USER ACCESS /')
     return render_template('index.html', OS=getOs, VERSION=getVersion, DATE=getDate, CPU=getCpu(), MEM=getMem())
 
+@app.route("/getcpu/", methods=['GET'])
 def getCpu():
     usageDelta = stats['cpu_stats']['cpu_usage']['total_usage'] - stats['precpu_stats']['cpu_usage']['total_usage']
     systemDelta = stats['cpu_stats']['system_cpu_usage'] - stats['precpu_stats']['system_cpu_usage']
     len_cpu = len(stats['cpu_stats']['cpu_usage']['percpu_usage'])
     percentage = (usageDelta / systemDelta) * len_cpu * 100
-    percent = round(percentage, 2)
+    percent = str(round(percentage, 2))
     return percent
 
+@app.route("/getmem/", methods=['GET'])
 def getMem():
     usage = stats['memory_stats']['usage']
-    convertion = usage / 1000000
+    convertion = str(usage / 1000000)
     return convertion
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
